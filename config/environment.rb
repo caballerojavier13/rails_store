92ff78e3a8cfc101f428eb8225825a987cef1e92
Adnane <ad.gasmi@gmail.com>
2014-07-12 16:30:49 -0400

    heroku mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+    ActionMailer::Base.smtp_settings = {
+      address:        "smtp.gmail.com",
+      port:           587, 
+      domain:         "google_domain",
+      authentication: "plain",
+      user_name:      "confirmation109",
+      password:       "Uottawa1",
+      enable_starttls_auto: true
+    } 
