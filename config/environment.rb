a56c5015b2342519c231192cc4ea85213b9c2081
Adnane <ad.gasmi@gmail.com>
2014-07-12 16:47:00 -0400

    heroku mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
+ ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-      address:        "smtp.gmail.com",
+      address:        "smtp.yahoo.ca",
       port:           587, 
-      domain:         "google_domain",
+      domain:         "yahoo_domain",
       authentication: "plain",
       user_name:      "confirmation109",
       password:       "Uottawa1",
