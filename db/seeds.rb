47edcc3b07dc63d749e0e638f01b19f7a800013e
Adnane <ad.gasmi@gmail.com>
2014-07-12 13:46:47 -0400

    seed file category

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,6 @@ Category.create(:title => "Vintage")
 Product.create (:title => "Picasso", 
 	:description=>"This is what's called abstract art It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using", 
 	:image_url=>"abstract2.jpg",
-	:price=>"50000.25",
-	:category_id=>"28")
+	:price=> 50000.25,
+	:category_id=> 28)
 
