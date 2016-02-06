1d8b0443e089898f5e7b2709f41c9cf2099e713a
Adnane <ad.gasmi@gmail.com>
2016-02-06 12:22:23 -0500

    update ruby version and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140627203904) do
 
-  create_table "cart_items", force: true do |t|
+  create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
     t.datetime "created_at"
@@ -26,44 +26,44 @@ ActiveRecord::Schema.define(version: 20140627203904) do
   add_index "cart_items", ["order_id"], name: "index_cart_items_on_order_id"
   add_index "cart_items", ["product_id"], name: "index_cart_items_on_product_id"
 
-  create_table "carts", force: true do |t|
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "categories", force: true do |t|
-    t.string   "title"
+  create_table "categories", force: :cascade do |t|
+    t.string   "title",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "orders", force: true do |t|
-    t.string   "name"
-    t.string   "country"
-    t.string   "province_state"
-    t.string   "city"
-    t.string   "postal_code"
+  create_table "orders", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.string   "country",        limit: 255
+    t.string   "province_state", limit: 255
+    t.string   "city",           limit: 255
+    t.string   "postal_code",    limit: 255
     t.text     "address1"
     t.text     "address2"
-    t.string   "email"
-    t.string   "pay_type"
+    t.string   "email",          limit: 255
+    t.string   "pay_type",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "products", force: true do |t|
-    t.string   "title"
+  create_table "products", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.text     "description"
-    t.string   "image_url"
+    t.string   "image_url",   limit: 255
     t.decimal  "price"
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "password_digest"
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "password_digest", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
